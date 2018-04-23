2a585812e4a4b27ba6af6c8b229a6ddb8c76df35
StepanSamsonov <Mynameisericlensherr@gmail.com>
2018-04-23 17:08:34 +0300

    Home page structure

--- src/app/parts/home/home.component.ts
+++ src/app/parts/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AngularFireDatabase} from "angularfire2/database";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  change_time_form: FormGroup;
+  time_mins: string;
+  time_hours: string;
+
+  constructor(private fb: FormBuilder,
+              private db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.createTimeForm();
+    this.db.list('const/time').push({mins: 0, hours: 12});
+    let data = this.db.object('const/time').valueChanges();
+    console.log(data);
+  }
+
+  createTimeForm() {
+    this.change_time_form = this.fb.group({
+      mins: ['', Validators.required],
+      hours: ['', Validators.required],
+    });
+  }
+
+  changeTime() {
+    let {mins, hours} = this.change_time_form.value;
+    if (mins != '') {
+      this.db.object('const/time').update({mins: mins});
+    }
+    if (hours != '') {
+      this.db.object('const/time').update({hours: hours});
+    }
   }
 
 }
--- src/app/parts/home/home.module.ts
+++ src/app/parts/home/home.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HomeComponent} from "./home.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {AngularFireModule} from "angularfire2";
+import {environment} from "../../../environments/environment";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   declarations: [HomeComponent],
   exports: [HomeComponent],
